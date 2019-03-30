ca7c2a2d0027c25e38ba08a1130239fa4ec866de
nbwns <nicolas.bauwens@gmail.com>
2019-03-30 22:11:12 +0100

    test netlify function

--- netlify-functions/timeline.js
+++ netlify-functions/timeline.js
@@ -1,6 +1,52 @@
-exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Hello, World" + event.queryStringParameters.lang
-    });
-}
+const express = require('express');
+const serverless = require('serverless-http');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors')
+const client = require('contentful')
+const moment = require('moment')
+
+app.use(bodyParser);
+app.use(cors())
+
+const client = contentful.createClient({
+    space: process.env.CTF_SPACE_ID,
+    accessToken: process.env.CTF_ACCESS_TOKEN
+})
+
+app.get('/timeline', (req, res, next) => {
+    
+    client.getEntries({
+        content_type: 'action',
+        'locale':req.query.lang+ "-BE",
+        'order': 'fields.sortOrder,fields.name'
+    })
+    .then(entries => {
+        let timelineItems = [];
+        entries.items.forEach(item => {
+
+            let newTimelineItem = {
+                "text": {
+                    "headline": item.fields.name,
+                    "text": item.fields.body
+                  }
+            }
+
+            if(item.fields.start){
+                let mDate = moment(item.fields.start)
+                let startDate = {
+                    "month": mDate.month(),
+                    "day": mDate.date(),
+                    "year": mDate.year()
+                }
+                newTimelineItem["start_date"] = startDate
+            }
+            
+            timelineItems.push(newTimelineItem)
+        })
+
+        res.json(timelineItems)
+    })
+})
+
+  module.exports.handler = serverless(app);
