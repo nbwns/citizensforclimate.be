9e5fab9e7086777ea09a1d1d67c381465bfb7377
Maël Brunet <maelbrunet@gmail.com>
2020-03-23 08:53:30 +0100

    Make header blue by default

--- components/Header.vue
+++ components/Header.vue
@@ -5,7 +5,7 @@
                 {{newLang.toUpperCase()}}
             </nuxt-link>
         </div>
-        <div class="hero-body" :class="{'bg-black': $route.params.slug, 'bg-blue': $route.params.locale}">
+        <div class="hero-body" :class="{'bg-black': $route.params.slug}">
             <div class="container has-text-centered">
             <nuxt-link :to="($route.params.locale ? '/' + $route.params.locale : '')">
                 <div class="logo" :class="{'is-white': $route.params.slug}">
@@ -40,12 +40,9 @@ export default {
     }
     .hero{
         position: relative;
-        background-color: white;
+        background-color: #64D7DE;
         color: black;
         height: 250px;
-        .bg-blue{
-            background-color: #64D7DE;
-        }
         .bg-black{
             background-color: black;
         }
